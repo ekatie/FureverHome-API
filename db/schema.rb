74d2deff4c1b1606b60472acd157d493f37ce0f3
Katie K <katiekucsera@gmail.com>
2024-03-01 16:35:53 -0500

    Updated schema after implementing devise auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_01_191952) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_01_204953) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -98,14 +98,22 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_01_191952) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", limit: 255, null: false
-    t.string "password_digest", null: false
     t.string "user_type", default: "0", null: false
     t.string "name", limit: 100, null: false
     t.date "date_of_birth", null: false
     t.string "phone", limit: 15, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.json "tokens"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "application_dog_matches", "applications"

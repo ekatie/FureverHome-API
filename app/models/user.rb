e2f7e6ed2243321f86ce044635c04de4d5c0691b
Katie K <katiekucsera@gmail.com>
2024-03-01 16:32:43 -0500

    Implement devise validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
-  has_secure_password
+  # Devise modules
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
 
+  # Associations
   has_many :applications, dependent: :destroy
   has_many :dogs, through: :applications
 
+  # Enums
   enum user_type: { adopter: 0, admin: 1 }
 
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  # Validations
   validates :name, presence: true, length: { maximum: 100 }
   validates :phone, presence: true, format: { with: /\A[0-9]+\z/ }, length: { maximum: 15 }
 

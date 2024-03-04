fd91bd7e82b22aacfdd1e330001ba2076f7ea8c8
Katie K <katiekucsera@gmail.com>
2024-03-04 16:59:10 -0500

    Update data types and valid values

--- db/migrate/20240301191708_create_dogs.rb
+++ db/migrate/20240301191708_create_dogs.rb
@@ -2,17 +2,17 @@ class CreateDogs < ActiveRecord::Migration[7.0]
   def change
     create_table :dogs do |t|
       t.string :name, null: false
-      t.string :age, null: false
+      t.decimal :age, null: false, precision: 4, scale: 1
       t.string :sex, null: false
       t.string :breed, null: false
-      t.string :size, null: false
+      t.integer :size, null: false
       t.text :description, null: false
       t.string :status, null: false, default: 'available'
       t.string :energy_level, null: false
       t.string :social_media_link
-      t.string :good_with_dogs, null: false, default: 'untested'
-      t.string :good_with_cats, null: false, default: 'untested'
-      t.string :good_with_kids, null: false, default: 'untested'
+      t.string :good_with_dogs, null: false, default: 'Untested'
+      t.string :good_with_cats, null: false, default: 'Untested'
+      t.string :good_with_kids, null: false, default: 'Untested'
       t.string :foster_location, null: false
       t.text :medical_conditions, null: false
       t.decimal :adoption_fee, null: false
@@ -22,9 +22,9 @@ class CreateDogs < ActiveRecord::Migration[7.0]
     end
     add_index :dogs, :default_image_id
 
-    add_check_constraint :dogs, "good_with_dogs IN ('yes', 'no', 'sometimes', 'untested')", name: 'check_good_with_dogs_values'
-    add_check_constraint :dogs, "good_with_cats IN ('yes', 'no', 'sometimes', 'untested')", name: 'check_good_with_cats_values'
-    add_check_constraint :dogs, "good_with_kids IN ('yes', 'no', 'sometimes', 'untested')", name: 'check_good_with_kids_values'
+    add_check_constraint :dogs, "good_with_dogs IN ('Yes', 'No', 'Sometimes', 'Untested')", name: 'check_good_with_dogs_values'
+    add_check_constraint :dogs, "good_with_cats IN ('Yes', 'No', 'Sometimes', 'Untested')", name: 'check_good_with_cats_values'
+    add_check_constraint :dogs, "good_with_kids IN ('Yes', 'No', 'Sometimes', 'Untested')", name: 'check_good_with_kids_values'
     add_check_constraint :dogs, "sex IN ('Male', 'Female')", name: 'check_dogs_sex'
     add_check_constraint :dogs, "status IN ('Available', 'Applications Being Reviewed', 'Pending Adoption', 'Adopted')", name: 'check_dogs_status'
     add_check_constraint :dogs, "energy_level IN ('Low', 'Medium', 'High', 'Very High')", name: 'check_dogs_energy_level'

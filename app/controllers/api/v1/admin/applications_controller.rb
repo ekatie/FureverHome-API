661f2f117730769c7095685f2649f417387849b0
Katie K <katiekucsera@gmail.com>
2024-03-07 18:32:30 -0500

    Update authentication actions

--- app/controllers/api/v1/admin/applications_controller.rb
+++ app/controllers/api/v1/admin/applications_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::Admin::ApplicationsController < ApplicationController
   # Ensure that the user is an admin before allowing them to access the applications
-  before_action :authenticate_admin!
+  before_action :authenticate_user!
+  before_action :check_admin!
 
   def index
   end

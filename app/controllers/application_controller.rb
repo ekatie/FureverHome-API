fb14e00da9620bfe54f77e2118480e48567237ea
Katie K <katiekucsera@gmail.com>
2024-03-07 18:37:47 -0500

    Implement authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
-include DeviseTokenAuth::Concerns::SetUserByToken
+
+def check_admin!
+  unless current_user.admin?
+    render json: { error: 'You are not authorized to perform this action' }, status: :forbidden
+  end
+end
 end

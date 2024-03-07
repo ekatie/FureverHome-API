0c5040ee38b26af20ba3dd2154b4a5248b856564
Katie K <katiekucsera@gmail.com>
2024-03-07 18:35:18 -0500

    Implement user update and params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,10 +13,32 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
+    if current_user.update(user_params)
+      render json: current_user
+    else
+      render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def show
     @user = User.find(params[:id])
     render json: @user, serializer: UserSerializer
+  end
+
+  private
+
+  def user_params
+    # List of base attributes
+    permitted_attributes = [:name, :email, :phone, :date_of_birth]
+    
+    # Only add password attributes if they are present in the request (indicating a desire to update the password)
+    if params[:user][:password].present? || params[:user][:password_confirmation].present?
+      permitted_attributes += [:password, :password_confirmation]
+    end
+    
+    # Permit the session parameter
+    permitted_attributes += [:session]
+
+    params.require(:user).permit(permitted_attributes)
   end
 end

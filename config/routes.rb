6dd22226491fff5beb714f94be4ab14775d4dc52
Katie K <katiekucsera@gmail.com>
2024-03-07 18:42:31 -0500

    Replace devise authentication with jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
+  get 'jwt_authentication/generate_token'
+  get 'jwt_authentication/verify_token'
+
   # Defines the root path route ("/")
   root 'home#index'
 
-  
-  namespace :api do
+    namespace :api do
     namespace :v1 do
-      
-      mount_devise_token_auth_for 'User', at: 'auth'
-      
+      # Authentication routes
+      post 'login', to: 'authentication#login'
+           
       # Static pages routes
       get 'static_pages/about'
       get 'static_pages/home'
-      
+
       # Application resource routes
       resources :applications, only: [:create, :show, :index] do
         member do
@@ -44,48 +46,3 @@ Rails.application.routes.draw do
   end
 
 end
-
-
-# Users (2 types – adopters and admin)
-# o	GET - /Users/:id/profile
-# 	Form to edit user profile
-# o	PUT - /Users/:id/profile
-# 	Update user profile
-# Applications
-# o	GET - /Application/new
-# 	Form to create a new application
-# o	GET - /Applications (admin)
-# 	To view all applications, allowing admin users to review and manage applications in various stages.
-# o	POST - /Applications
-# 	Submit a new application
-# o	GET - /Application/:id
-# 	View a specific application’s details
-# o	POST - /Applications/:id/cancel
-# 	Allow adopters to cancel their application.
-# o	POST - /Applications/:id/select-match
-# 	For adopters to select a dog from matches to move forward with the application.
-# o	POST - /Applications/:id/schedule/interview
-# 	Schedule an interview
-# o	POST - /Applications/:id/schedule/virtual-meet-greet
-# 	Schedule a virtual meet & greet
-# o	POST - /Applications/:id/schedule/adoption-day
-# 	Schedule the adoption day
-# o	GET - /Applications/:id/matches
-# 	View dog matches for the application based on the matching criteria
-# Dogs
-# o	GET - /Dogs/new (admin)
-# 	Form to add new dog
-# o	POST - /Dogs (admin)
-# 	Submit a new dog
-# o	GET - /Dogs
-# 	View all dogs
-# o	GET - /Dogs/:id/edit (admin)
-# 	Form to edit a dog’s details
-# o	PUT - /Dogs/:id/edit (admin)
-# 	Update a dog’s details
-# o	GET - /Dogs/favourites
-# 	View logged in user’s favourite dogs
-# o	GET - /Dogs/:id
-# 	View a specific dog’s details
-# o	POST - /Dogs/:id/favourite
-# 	Toggle the favourite status of a dog for the logged-in user, combining the creation and deletion of favourites into one action based on current state. */

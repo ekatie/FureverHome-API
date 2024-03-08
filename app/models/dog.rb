f69d223bb8987d93e0e22917a57392b3ca1d7c32
Katie K <katiekucsera@gmail.com>
2024-03-08 18:51:40 -0500

    Add favourite related associations

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,14 +1,19 @@
 class Dog < ApplicationRecord
+  # Constants
   VALID_SEXES = ['Male', 'Female'].freeze
   VALID_STATUSES = ['Available', 'Applications Being Reviewed', 'Pending Adoption', 'Adopted'].freeze
   VALID_ENERGY_LEVELS = ['Low', 'Medium', 'High', 'Very High'].freeze
   VALID_GOOD_WITH_VALUES = ['Yes', 'No', 'Sometimes', 'Untested'].freeze
 
+  # Associations
   has_many :dog_images, dependent: :destroy
   has_many :applications, dependent: :destroy
   has_many :users, through: :applications
   
+  has_many :favourites, dependent: :destroy
+  has_many :favourited_by_users, through: :favourites, source: :user
 
+  # Validations
   validates :name, presence: true
   validates :age, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 20.0, message: "must be between 0 and 20.0" }
   validates :sex, presence: true, inclusion: { in: VALID_SEXES }
@@ -23,7 +28,6 @@ class Dog < ApplicationRecord
   validates :foster_location, presence: true
   validates :medical_conditions, presence: true
   validates :adoption_fee, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  
   
   def default_image_url
     dog_images.find_by(is_default: true)&.url

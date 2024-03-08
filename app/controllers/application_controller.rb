ad9db8f3cdaea428e3d658525e137be1462e9f44
Katie K <katiekucsera@gmail.com>
2024-03-08 18:48:23 -0500

    Move authentication methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,35 @@
 class ApplicationController < ActionController::API
 
-def check_admin!
-  unless current_user.admin?
-    render json: { error: 'You are not authorized to perform this action' }, status: :forbidden
+  def index
   end
-end
+
+
+  private
+
+    def check_adopter!
+      unless current_user.adopter?
+        render json: { error: 'Only adopters can perform this action' }, status: :forbidden
+      end
+    end
+
+    def authenticate_user!
+      token = request.headers['Authorization']&.split(' ')&.last
+      Rails.logger.info "Received Token: #{token}"
+      decoded_token = JwtAuthenticationController.new.verify_token(token)
+      Rails.logger.info "Decoded Token: #{decoded_token}"
+      @current_user = User.find(decoded_token['user_id']) if decoded_token
+      Rails.logger.info "Current User: #{@current_user}"
+
+
+      token = request.headers['Authorization']&.split(' ')&.last
+      decoded_token = JwtAuthenticationController.new.verify_token(token)
+      @current_user = User.find(decoded_token['user_id']) if decoded_token
+      render json: { error: 'Unauthorized' }, status: :unauthorized unless @current_user
+    end
+
+    def check_admin!
+      unless current_user.admin?
+        render json: { error: 'You are not authorized to perform this action' }, status: :forbidden
+      end
+    end
 end

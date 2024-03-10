fd1ed04cd65df8f27a56c7049b17fc4a7ab64804
Katie K <katiekucsera@gmail.com>
2024-03-10 18:45:15 -0400

    Implement set current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,13 +12,17 @@ class ApplicationController < ActionController::API
       end
     end
 
-    def authenticate_user!
+    def set_current_user
       token = request.headers['Authorization']&.split(' ')&.last
       Rails.logger.info "Received Token: #{token}"
       decoded_token = JwtAuthenticationController.new.verify_token(token)
       Rails.logger.info "Decoded Token: #{decoded_token}"
       @current_user = User.find(decoded_token['user_id']) if decoded_token
       Rails.logger.info "Current User: #{@current_user}"
+    end
+
+    def authenticate_user!
+      set_current_user
       render json: { error: 'Unauthorized' }, status: :unauthorized unless @current_user
     end
 

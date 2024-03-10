1f95152ef29b831f08d4dbc61054cdb6365055cf
Katie K <katiekucsera@gmail.com>
2024-03-10 18:46:47 -0400

    Implement is favourite method

--- app/serializers/dog_serializer.rb
+++ app/serializers/dog_serializer.rb
@@ -1,7 +1,12 @@
 class DogSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :sex, :breed, :size, :description, :status, :energy_level, :foster_location, :medical_conditions, :adoption_fee, :default_image_url
+  attributes :id, :name, :age, :sex, :breed, :size, :description, :status, :energy_level, :foster_location, :medical_conditions, :adoption_fee, :default_image_url, :is_favourite
 
   def default_image_url
     object.dog_images.find_by(is_default: true)&.url
+  end
+
+  def is_favourite
+    Rails.logger.info "Current scope (user): #{scope.inspect}"
+    object.is_favourite(scope)
   end
 end

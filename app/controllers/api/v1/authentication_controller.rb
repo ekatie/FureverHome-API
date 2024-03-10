251be30b4527670473559cdf4c57a81808666dfe
Katie K <katiekucsera@gmail.com>
2024-03-10 18:44:50 -0400

    Implement validate token

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -8,4 +8,21 @@ class Api::V1::AuthenticationController < ApplicationController
       render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
   end
+
+  def validate_token
+    token = params[:token] || request.headers['Authorization']&.split(' ')&.last
+    decoded_token = JwtAuthenticationController.new.verify_token(token)
+    if decoded_token
+      render json: { valid: true, user_id: decoded_token['user_id'] }, status: :ok
+    else
+      render json: { valid: false }, status: :unauthorized
+    end
+  rescue JWT::ExpiredSignature
+    # Handle expired token
+    render json: { valid: false, error: "Token has expired" }, status: :unauthorized
+  rescue JWT::DecodeError => e
+    # Handle other errors, such as invalid token
+    render json: { valid: false, error: e.message }, status: :unauthorized
+  end
+  
 end

cc59a432b90e5acd8d26378f957f1dcedad6b4d4
Katie K <katiekucsera@gmail.com>
2024-03-13 18:54:32 -0400

    Add all dog images to dog serializer

--- app/serializers/dog_serializer.rb
+++ app/serializers/dog_serializer.rb
@@ -1,8 +1,12 @@
 class DogSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :sex, :breed, :size, :description, :status, :energy_level, :foster_location, :medical_conditions, :adoption_fee, :default_image_url, :is_favourite
+  attributes :id, :name, :age, :sex, :breed, :size, :description, :status, :energy_level, :foster_location, :medical_conditions, :adoption_fee, :default_image_url, :is_favourite, :images
 
   def default_image_url
     object.dog_images.find_by(is_default: true)&.url
+  end
+
+  def images
+    object.dog_images.pluck(:url)
   end
 
   def is_favourite

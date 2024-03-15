7ab4c7ea3e45139f6ca2a3b2e45d01752304256a
Katie K <katiekucsera@gmail.com>
2024-03-15 14:53:14 -0400

    Implement admin dogs methods

--- app/controllers/api/v1/admin/dogs_controller.rb
+++ app/controllers/api/v1/admin/dogs_controller.rb
@@ -1,14 +1,37 @@
-class Api::V1::Admin::ApplicationsController < ApplicationController
+class Api::V1::Admin::DogsController < ApplicationController
   # Ensure that the user is an admin before allowing them to access the applications
   before_action :authenticate_user!
   before_action :check_admin!
 
   def index
+    @dogs = Dog.all
+    render json: @dogs, each_serializer: DogSerializer
   end
 
   def show
+    @dog = Dog.find(params[:id])
+    render json: @dog, serializer: DogSerializer, status: :ok
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Dog not found' }, status: :not_found
+  end
+
+  def create
   end
 
   def update
+    @dog = Dog.find(params[:id])
+    if @dog.update(dog_params)
+      render json: @dog, serializer: DogSerializer, status: :ok
+    else
+      render json: { errors: @dog.errors.full_messages }, status: :unprocessable_entity
+    end
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Dog not found' }, status: :not_found
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :age, :sex, :breed, :size, :description, :status, :energy_level, :foster_location, :medical_conditions, :adoption_fee, :good_with_cats, :good_with_dogs, :good_with_kids, :social_media_link)
   end
 end

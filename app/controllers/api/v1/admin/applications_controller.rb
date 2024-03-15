9f690a3fb337fc3bdff1020a019dd2184afb0343
Katie K <katiekucsera@gmail.com>
2024-03-15 14:52:50 -0400

    Impement admin applications methods

--- app/controllers/api/v1/admin/applications_controller.rb
+++ app/controllers/api/v1/admin/applications_controller.rb
@@ -4,11 +4,32 @@ class Api::V1::Admin::ApplicationsController < ApplicationController
   before_action :check_admin!
 
   def index
+    @applications = Application.all
+    render json: @applications, each_serializer: ApplicationSerializer, status: :ok
   end
 
   def show
+    @application = Application.find(params[:id])
+    render json: @application, serializer: ApplicationSerializer, status: :ok
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Application not found' }, status: :not_found
   end
 
   def update
+    @application = Application.find(params[:id])
+    if @application.update(application_params)
+      render json: @application, serializer: ApplicationSerializer, status: :ok
+    else
+      render json: { errors: application.errors.full_messages }, status: :unprocessable_entity
+    end
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Application not found' }, status: :not_found
   end
+
+  private
+
+  def application_params
+    params.require(:application).permit(:status)
+  end
+  
 end

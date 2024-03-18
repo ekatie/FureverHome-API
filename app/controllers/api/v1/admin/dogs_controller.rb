0c607af45612b2332b15938c4603316f1c774e80
Katie K <katiekucsera@gmail.com>
2024-03-18 14:45:33 -0400

    Update dog image params

--- app/controllers/api/v1/admin/dogs_controller.rb
+++ app/controllers/api/v1/admin/dogs_controller.rb
@@ -26,8 +26,11 @@ class Api::V1::Admin::DogsController < ApplicationController
 
   def update
     @dog = Dog.find(params[:id])
+
     ActiveRecord::Base.transaction do
       if @dog.update(dog_params)
+
+        @dog.manage_default_image  # Call method to manage default images
         render json: @dog, serializer: DogSerializer, status: :ok
       else
         render json: { errors: @dog.errors.full_messages }, status: :unprocessable_entity
@@ -36,13 +39,14 @@ class Api::V1::Admin::DogsController < ApplicationController
     end
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'Dog not found' }, status: :not_found
+  rescue => e
+    render json: { error: e.message }, status: :unprocessable_entity
   end
-  
 
   private
 
   def dog_params
-    params.require(:dog).permit(:name, :age, :sex, :breed, :size, :description, :status, :energy_level, :foster_location, :medical_conditions, :adoption_fee, :good_with_cats, :good_with_dogs, :good_with_kids, :social_media_link, dog_images_attributes: [:id, :url, :is_default, :_destroy])
+    params.require(:dog).permit(:name, :age, :sex, :breed, :size, :description, :status, :energy_level, :foster_location, :medical_conditions, :adoption_fee, :good_with_cats, :good_with_dogs, :good_with_kids, :social_media_link, :default_image_url, :is_favourite, dog_images_attributes: [:id, :url, :is_default, :_destroy])
   end
 
 end

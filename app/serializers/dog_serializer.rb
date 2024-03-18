78ffd5f91b474693b3b590d333e98df1c288838f
Katie K <katiekucsera@gmail.com>
2024-03-18 14:47:02 -0400

    Update dog serializer for image params

--- app/serializers/dog_serializer.rb
+++ app/serializers/dog_serializer.rb
@@ -1,24 +1,18 @@
 class DogSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :sex, :breed, :size, :description, :status, :energy_level, :foster_location, :medical_conditions, :adoption_fee, :default_image_url, :is_favourite, :images, :good_with_cats, :good_with_dogs, :good_with_kids, :social_media_link
-
-  def default_image_url
-    object.dog_images.find_by(is_default: true)&.url
-  end
-
-  def images
-    object.dog_images.pluck(:url)
-  end
+  attributes :id, :name, :age, :sex, :breed, :size, :description, :status, :energy_level, :foster_location, :medical_conditions, :adoption_fee, :is_favourite, :dog_images_attributes, :good_with_cats, :good_with_dogs, :good_with_kids, :social_media_link
 
   def is_favourite
     Rails.logger.info "Current scope (user): #{scope.inspect}"
     object.is_favourite(scope)
   end
 
-  def images
+  def dog_images_attributes
     object.dog_images.map do |image|
       {
+        id: image.id,
         url: image.url,
         is_default: image.is_default,
+        _destroy: false
       }
     end
   end

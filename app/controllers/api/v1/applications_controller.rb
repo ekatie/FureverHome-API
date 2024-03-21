b067ad18df5ed0d30db3ecc00fb504f1b5fc798f
Katie K <katiekucsera@gmail.com>
2024-03-21 14:54:03 -0400

    Update params and auth, implement cancellation and booking methods

--- app/controllers/api/v1/applications_controller.rb
+++ app/controllers/api/v1/applications_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::ApplicationsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :update, :cancel, :show, :schedule_interview, :schedule_virtual_meet_greet, :schedule_adoption_day, :matches]
-  before_action :check_adopter!, only: [:create, :cancel, :schedule_interview, :schedule_virtual_meet_greet, :schedule_adoption_day, :matches]
+  before_action :authenticate_user!, only: [:create, :update, :cancel, :show, :booking, :matches]
+  before_action :check_adopter!, only: [:create, :cancel, :booking, :matches]
 
+  require 'net/http'
+  require 'uri'
+  require 'base64'
+  
   def new
   end
 
@@ -39,15 +43,17 @@ end
   end
   
   def cancel
+    application = current_user.applications.find(params[:id])
+  
+  if application.update(status: 'Cancelled')
+    dog = application.dog
+    if dog && dog.applications.where.not(status: 'Cancelled').empty?
+      dog.update(status: 'Available')
+    end
+    render json: application, status: :ok
+  else
+    render json: { errors: application.errors.full_messages }, status: :unprocessable_entity
   end
-
-  def schedule_interview
-  end
-
-  def schedule_virtual_meet_greet
-  end
-
-  def schedule_adoption_day
   end
 
   def matches
@@ -70,10 +76,61 @@ end
   render json: { error: 'Application not found' }, status: :not_found
 end
 
+# Fetch event details from Calendly
+def fetch_calendly_event_details(uuid)
+  uri = URI.parse("https://api.calendly.com/scheduled_events/#{uuid}")
+
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+
+  request = Net::HTTP::Get.new(uri.request_uri)
+  request["Authorization"] = "Bearer #{ENV['CALENDLY_PERSONAL_ACCESS_TOKEN']}"
+
+  response = http.request(request)
+
+  if response.code == "200"
+    JSON.parse(response.body)
+  else
+    Rails.logger.error "Failed to fetch Calendly event details. Response code: #{response.code}, Body: #{response.body}"
+    nil
+  end
+end
+
+# Update application with Calendly event details
+def booking
+  application_id = params[:id]
+  event_uri = params[:event_uri]
+  application = current_user.applications.find(application_id)
+
+  # Extract UUID from event URI
+  uuid = event_uri.split('/').last
+
+  # User personal access token to fetch event details
+  event_details = fetch_calendly_event_details(uuid)
+
+  if event_details && event_details["resource"]
+    start_time_str = event_details["resource"]["start_time"]
+    unless start_time_str.nil?
+      start_time = DateTime.parse(start_time_str)
+
+  # Update the application based on its current status
+  case application.status
+  when "Pending Interview Booking"
+    application.update(interview_date: start_time, status: 'Interview Booked')
+  when "Pending Meet and Greet Booking"
+    application.update(meet_greet_date: start_time, status: 'Meet and Greet Booked')
+  when "Pending Adoption Date Booking"
+    application.update(adoption_date: start_time, status: 'Adoption Date Booked')
+  else
+    # Log or handle unexpected application status
+    Rails.logger.info "Unexpected application status: #{application.status}"
+  end
+end
+
   private
 
   def application_params
-    params.require(:application).permit(:status, :read_profile, :address, :current_pets, :current_pets_details, :felony_conviction, :felony_details, :pet_prohibition, :prohibition_details, :previous_adoption, :adoption_details, :residence_type, :landlord_permission, :occupation, :adoption_reason, :dog_experience, :stimulation_plan, :household_children, :household_allergies, :household_agreement, :sleeping_arrangement, :vet_frequency, {dog_age: []}, {dog_size: []}, :dog_energy_level, :dog_medical_conditions, :dog_id)
+    params.require(:application).permit(:status, :read_profile, :address, :current_pets, :current_pets_details, :felony_conviction, :felony_details, :pet_prohibition, :prohibition_details, :previous_adoption, :adoption_details, :residence_type, :landlord_permission, :occupation, :adoption_reason, :dog_experience, :stimulation_plan, :household_children, :household_allergies, :household_agreement, :sleeping_arrangement, :vet_frequency, {dog_age: []}, {dog_size: []}, :dog_energy_level, :dog_medical_conditions, :dog_id, :interview_date, :meet_greet_date, :adoption_date)
   end
 
 end

e69e048e225d4ae0dc87387d1b3cd019d2c99f9a
Katie K <katiekucsera@gmail.com>
2024-03-24 15:26:53 -0400

    Remove logging

--- app/serializers/dog_serializer.rb
+++ app/serializers/dog_serializer.rb
@@ -2,7 +2,6 @@ class DogSerializer < ActiveModel::Serializer
   attributes :id, :name, :age, :sex, :breed, :size, :description, :status, :energy_level, :foster_location, :medical_conditions, :adoption_fee, :is_favourite, :dog_images_attributes, :good_with_cats, :good_with_dogs, :good_with_kids, :social_media_link
 
   def is_favourite
-    Rails.logger.info "Current scope (user): #{scope.inspect}"
     object.is_favourite(scope)
   end
 
